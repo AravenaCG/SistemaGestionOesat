04d441ef18b5b0163a896a32363378fd60427a56
caravena <caravena@exisoft.com.ar>
2024-03-14 14:45:50 -0300

    comento el .collapse en app.css

--- SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
+++ SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a8202d812c12b2aa65402c1a80588435aa583a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c4d0275eba521f5c3b27017354e2905d1701e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
