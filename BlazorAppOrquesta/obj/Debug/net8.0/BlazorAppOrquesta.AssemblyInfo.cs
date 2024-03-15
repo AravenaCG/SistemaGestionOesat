70fada45f086f90be73fc5e0967f6a60a7b80ba4
caravena <caravena@exisoft.com.ar>
2024-03-15 15:31:38 -0300

    cambios para el js en estudiante.razor e index.html corrijo path

--- BlazorAppOrquesta/obj/Debug/net8.0/BlazorAppOrquesta.AssemblyInfo.cs
+++ BlazorAppOrquesta/obj/Debug/net8.0/BlazorAppOrquesta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppOrquesta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c576363b278014c8f1f756b0a41f9c5111cbe390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90564a800e56624b0a69dfba43a0a555857071a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppOrquesta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppOrquesta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
+++ SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c576363b278014c8f1f756b0a41f9c5111cbe390")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90564a800e56624b0a69dfba43a0a555857071a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
