155801ba00cc82e87d8aef11b3494521083afb22
caravena <caravena@exisoft.com.ar>
2024-06-06 01:54:52 -0300

    pruebo cambio versiones .net -

--- BlazorAppOrquesta/obj/Debug/net8.0/BlazorAppOrquesta.AssemblyInfo.cs
+++ BlazorAppOrquesta/obj/Debug/net8.0/BlazorAppOrquesta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppOrquesta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+723867e79220a6307f4bbeb61cee029776bfacf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21182cbdea3d1b35c942932e313181a340de78f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppOrquesta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppOrquesta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
+++ SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+723867e79220a6307f4bbeb61cee029776bfacf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21182cbdea3d1b35c942932e313181a340de78f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
