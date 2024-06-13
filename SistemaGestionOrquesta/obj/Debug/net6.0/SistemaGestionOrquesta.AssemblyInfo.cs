752dfaebc6bb2f5d599021cb5aedfacd11d22863
caravena <caravena@exisoft.com.ar>
2024-06-13 16:01:35 -0300

    cambio version .net

--- SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
+++ SistemaGestionOrquesta/obj/Debug/net6.0/SistemaGestionOrquesta.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e1093fc3b9208d7b7bca6a57b5a9724d286385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd9aa51060130f28a60b86608a2c6de53d16c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionOrquesta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
